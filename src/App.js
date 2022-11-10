7c33b21c20370521b8ed74793b956efc3789e705
Thomas Weigert <thomasweigert91@gmail.com>
2022-11-10 17:01:33 +0100

    added fetch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import "./App.css";
 import React from "react";
+import styled from "styled-components";
+import { useEffect } from "react";
 import { Header } from "./content/Header/Header";
-import { Body } from "./content/Body/Body";
 import { Navigation } from "./content/Navigation/Navigation";
+import { Home } from "./content/Main/Home";
 
 function App() {
+  useEffect(() => {
+    fetchData();
+  }, []);
+  async function fetchData() {
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await response.json();
+    console.log(data.results);
+  }
+
   return (
     <div className="App">
       <Header />
-      <Body />
+      <Main>
+        <Home />
+      </Main>
       <Navigation />
     </div>
   );
 }
+
+const Main = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+`;
 
 export default App;

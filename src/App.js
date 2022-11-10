3ab9ad4fcdf6e3401aca457f92c642fac628902b
Thomas Weigert <thomasweigert91@gmail.com>
2022-11-10 16:17:32 +0100

    added header, body, footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Header } from "./content/Header/Header";
+import { Body } from "./content/Body/Body";
+import { Navigation } from "./content/Navigation/Navigation";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Body />
+      <Navigation />
     </div>
   );
 }
